291fea34f6599eb46126b971a45717a8073f396c
jcuenod <4253884+jcuenod@users.noreply.github.com>
2025-07-15 15:22:44 -0500

    Add context for build and user/group in start script

---
+++ start.sh
@@ -0,0 +1,19 @@
+export UID=$(id -u)
+export GID=$(id -g)
+mkdir -p ${HOME}/.silauto/db
+
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+echo "========== Starting SIL Auto Docker Build =========="
+echo "SSH_CONTAINER_KEY: $SSH_CONTAINER_KEY"
+echo "USFM2PDF_PATH:     $USFM2PDF_PATH"
+echo "SILNLP_ROOT:       $SILNLP_ROOT"
+echo "SILNLP_DATA:       $SILNLP_DATA"
+echo "MAX_CONCURRENT_FILE_PROCESSING: $MAX_CONCURRENT_FILE_PROCESSING"
+echo "Docker build is starting in 2 seconds..."
+echo "===================================================="
+sleep 2
+
+docker compose up --build
